d851a70434020bee7e5b251288c7ac8d1fdff4f9
Andra <andrawoods98@gmail.comm>
2021-03-02 20:54:35 +0000

    Prompt user for name and greet them

--- game.py
+++ game.py
@@ -1,2 +1,8 @@
 """ A number-guessing game."""
 print('Hi')
+#Prompt user for a name
+user = input("What is your name: ")
+print("Hello, " + str(user) + ". We are guessing between a number between 1 and 100")
+#While the guess is not correct prompt for a guess
+#If the guess is too low say so or too high say so.
+#If the guess is correct print so and exit the loop 
